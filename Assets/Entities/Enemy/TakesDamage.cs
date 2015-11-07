8778d795a4e9aeceac7246a8d4b0e948990a3f6f
Trevor Redfern <trevorredfern@gmail.com>
2015-11-07 10:32:23 -0800

    Updated some images and general graphics. Renamed project

---
+++ Assets/Entities/Enemy/TakesDamage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakesDamage : MonoBehaviour {
+	public float Health = 10.0f;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Projectile missile = other.GetComponent<Projectile> ();
+		if (missile) {
+			TakeDamage(missile.GetDamage ());
+			missile.Hit();
+		}
+	}
+
+	private void TakeDamage(float damage) {
+		this.Health -= damage;
+
+		if (Health <= 0) {
+			Destroy (this.gameObject);
+		}
+
+	}
+}
---
+++ Assets/Entities/Projectile/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float Damage = 100.0f;
+
+	public float GetDamage() {
+		return Damage;
+	}
+
+	public void Hit() {
+		Destroy (gameObject);
+	}
+}
