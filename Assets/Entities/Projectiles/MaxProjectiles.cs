23d8bd6fb2cc7545d62d4a00078365f64acefe3a
Trevor Redfern <trevorredfern@gmail.com>
2015-11-08 20:12:53 -0800

    Added simple script that limits the number of enemy projectiles

---
+++ Assets/Entities/Projectiles/MaxProjectiles.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple script to limit how many projectiles are on a screen at a time
+/// </summary>
+public class MaxProjectiles : MonoBehaviour {
+	public int maximum = 100;
+
+	// Use this for initialization
+	void Start () {
+		int currentCount = FindObjectsOfType<MaxProjectiles> ().Length;
+		if (currentCount > maximum)
+			Destroy (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
