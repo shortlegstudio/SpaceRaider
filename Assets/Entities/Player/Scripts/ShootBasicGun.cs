0b699ff98891fcdc86b67915804ddff6cdf5c848
Trevor Redfern <trevorredfern@gmail.com>
2015-11-27 09:26:16 -0800

    Makes different projectiles as you level up

--- Assets/Entities/Player/Scripts/ShootBasicGun.cs
+++ Assets/Entities/Player/Scripts/ShootBasicGun.cs
@@ -10,6 +10,14 @@ public class ShootBasicGun : MonoBehaviour {
 	public float FireRatePerLevel = 0.01f;
 	private int WeaponLevel = 0;
 	private int MaxLevel = 10;
+	private float offsetX = 0;
+	private int extraLaserLevel = 2;
+
+	void Start() {
+		SpriteRenderer render = GetComponentInParent<SpriteRenderer> ();
+		offsetX = render.sprite.pivot.x / render.sprite.pixelsPerUnit;
+	}
+
 
 	// Update is called once per frame
 	void Update () {
@@ -32,14 +40,48 @@ public class ShootBasicGun : MonoBehaviour {
 	/// Creates a new Laser entity
 	/// </summary>
 	private void FireLaser() {
-		GameObject laser = (GameObject)Instantiate (LaserPrefab, transform.position, Quaternion.identity);
+		int laserCounts = WeaponLevel / extraLaserLevel;
+
+		//Player has earned extra lasers
+		if (laserCounts > 0 && WeaponLevel < MaxLevel) {
+			float xInc = (offsetX * 2) / laserCounts;
+			Vector3 startPosition = transform.position;
+			startPosition.x -= offsetX; //Farthest left
+			Debug.Log ("xIncrement: " + xInc);
+			for (int index=0; index <= WeaponLevel / extraLaserLevel; index++) {
+				CreateProjectile (startPosition);
+				startPosition.x += xInc;
+			}
+		} else if (WeaponLevel == MaxLevel) {
+			//Make a giant laser shot
+			CreateMassiveProjectile(transform.position);
+
+		} else {
+			//just create a single laser shot
+			CreateProjectile (transform.position);
+		}
+
+		AudioSource.PlayClipAtPoint (LaserSound, this.transform.position, LaserVolume);
+	}
+
+	private void CreateProjectile(Vector3 pos) {
+		GameObject laser = (GameObject)Instantiate (LaserPrefab, pos, Quaternion.identity);
 		Projectile proj = laser.GetComponent<Projectile> ();
 		proj.Damage += DamagePerLevel * WeaponLevel;
 		Vector3 scale = Vector3.one;
 		scale.y += 0.05f * WeaponLevel;
 		scale.x += 0.025f * WeaponLevel;
 		proj.transform.localScale = scale;
-		AudioSource.PlayClipAtPoint (LaserSound, this.transform.position, LaserVolume);
+	}
+
+	private void CreateMassiveProjectile(Vector3 pos) {
+		GameObject laser = (GameObject)Instantiate (LaserPrefab, pos, Quaternion.identity);
+		Projectile proj = laser.GetComponent<Projectile> ();
+		proj.Damage += DamagePerLevel * WeaponLevel;
+		Vector3 scale = Vector3.one;
+		scale.y += 1;
+		scale.x += 3;
+		proj.transform.localScale = scale;
 	}
 
 	public void UpgradeWeapon() {
